7f2198fee1afc68ac1b861c04a80939da881702d
SayYoungMan <nickte89@gmail.com>
2023-11-26 19:19:37 +0000

    feat: MusicMetaData class to store music video info

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ import googleapiclient.discovery
 
 DAILY_PLAYLIST_ID = "PLXzLX2ct6ysab-Gy0b1Xrm9Ka-Pg-yqmR"
 
+class MusicMetaData:
+    def __init__(self, playlist_item) -> None:
+        # Snippet is a part of playlistItem that has all relevant information
+        snippet = playlist_item["snippet"]
+
+        self.title = snippet["title"]
+        self.position = snippet["position"]
+        self.owner = snippet["videoOwnerChannelTitle"].removesuffix(" - Topic").strip()
+        self.image = snippet["thumbnails"]["high"]["url"]
+
+    def __repr__(self) -> str:
+        return f"{self.title} by {self.owner}"
+    
+
 def get_youtube_api_key() -> str:
     load_dotenv()
     return os.environ["YOUTUBE_API_KEY"]
@@ -27,20 +41,27 @@ def fetch_playlist_items(client, next_page_token):
 
     return response
 
-def fetch_all_playlist_items(client):
+def is_private_video(playlist_item):
+    return playlist_item["snippet"]["title"] == "Private video"
+
+def fetch_all_playlist_items(client) -> list[MusicMetaData]:
     all_items = []
     next_page_token = None
 
+    # Loop to get all videos in the playlist
     while True:
         response = fetch_playlist_items(client, next_page_token)
         all_items.extend(response["items"])
 
+        # If there is next page, fetch next page, otherwise break and return all items
         if "nextPageToken" in response:
             next_page_token = response["nextPageToken"]
         else:
             break
 
-    return all_items
+    all_musics = [MusicMetaData(item) for item in all_items if not is_private_video(item)]
+
+    return all_musics
 
 def main():
     youtube = get_youtube_client()
