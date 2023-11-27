58ab3c898c3e79b6832f610f5b5c3ed7c4cb0bd6
SayYoungMan <nickte89@gmail.com>
2023-11-27 23:06:18 +0000

    fix: get last collection instead of yesterday

--- src/main.py
+++ src/main.py
@@ -5,17 +5,17 @@ from dotenv import load_dotenv
 
 from music import MusicMetaData
 from youtube import get_youtube_client, fetch_all_playlist_items
-from mongodb import get_mongo_client, write_music_data_to_db, get_yesterdays_music_titles
+from mongodb import get_mongo_client, write_music_data_to_db, get_last_music_titles
 
-def is_changed_today(yesterday_music_dict: dict[str, bool], today_music_data: list[MusicMetaData]) -> bool:
-    yesterday_music_count = len(yesterday_music_dict.keys())
+def is_changed_today(last_music_dict: dict[str, bool], today_music_data: list[MusicMetaData]) -> bool:
+    last_music_count = len(last_music_dict.keys())
     today_music_count = len(today_music_data)
-    if yesterday_music_count != today_music_count:
-        logging.info("Number of songs today is different [yesterday:%i, today:%i]", yesterday_music_count, today_music_count)
+    if last_music_count != today_music_count:
+        logging.info("Number of songs today is different [last:%i, today:%i]", last_music_count, today_music_count)
         return True
 
     for music in today_music_data:
-        if music.title not in yesterday_music_dict:
+        if music.title not in last_music_dict:
             logging.info("There is a song whose title was not there yesterday: %s", music.title)
             return True
         
@@ -29,10 +29,10 @@ def main():
     youtube = get_youtube_client()
     mongo = get_mongo_client()
 
-    yesterday_music_dict = get_yesterdays_music_titles(mongo)
+    last_music_dict = get_last_music_titles(mongo)
     today_music_data = fetch_all_playlist_items(youtube)
 
-    if is_changed_today(yesterday_music_dict, today_music_data):
+    if is_changed_today(last_music_dict, today_music_data):
         write_music_data_to_db(mongo, today_music_data)    
 
 if __name__ == "__main__":
--- src/mongodb.py
+++ src/mongodb.py
@@ -18,10 +18,11 @@ def get_mongo_client() -> MongoClient:
 def _get_mongo_db(client: MongoClient) -> Database:
     return client[DATABASE_NAME]
 
-def _get_yesterday_collection(db: Database) -> Collection:
-    yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+def _get_last_collection(db: Database) -> Collection:
+    collection_names = db.list_collection_names()
+    latest_collection_name = max(collection_names)
 
-    return db.get_collection(yesterday)
+    return db.get_collection(latest_collection_name)
 
 def _create_collection(db: Database, collection_name: str) -> Collection:
     return db.create_collection(collection_name)
@@ -45,13 +46,13 @@ def write_music_data_to_db(client: MongoClient, music_data: list[MusicMetaData])
 
     _write_music_documents(collection, music_data)
 
-def get_yesterdays_music_titles(client: MongoClient) -> dict[str, bool]:
+def get_last_music_titles(client: MongoClient) -> dict[str, bool]:
     db = _get_mongo_db(client)
-    yesterday_collection = _get_yesterday_collection(db)
-    yesterday_music_data = list(yesterday_collection.find())
+    last_collection = _get_last_collection(db)
+    last_music_data = list(last_collection.find())
 
     res = {}
-    for data in yesterday_music_data:
+    for data in last_music_data:
         res[data["title"]] = True
 
     return res
